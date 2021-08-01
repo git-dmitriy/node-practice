5b6a2d33e4c1151dcb153c28914fd0ff96a818d8
git-dmitriy <shalberkind@mail.ru>
2021-08-01 23:56:00 +0300

    Understanding requests

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.headers, req.method);
+  // process.exit();
 });
 
 server.listen(3000);
