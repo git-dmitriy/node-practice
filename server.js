21a777528c82d573aebcf0061b800b63ef43e9ba
git-dmitriy <shalberkind@mail.ru>
2021-08-02 20:31:03 +0300

    Using node module system

--- server.js
+++ server.js
@@ -1,39 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const routes = require('./routes');
 
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write(
-      '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Message</title></head><body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">send</button></form></body></html>'
-    );
-    return res.end();
-  }
-
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('messages.txt', message, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write(
-    '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body><h1>Hello from Node.js server!</h1></body></html>'
-  );
-  res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
