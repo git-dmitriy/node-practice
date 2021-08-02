e51beb06efe683d8d68f8ac76520c534fba84425
git-dmitriy <shalberkind@mail.ru>
2021-08-02 19:29:14 +0300

    Non blocking code

--- server.js
+++ server.js
@@ -18,14 +18,15 @@ const server = http.createServer((req, res) => {
     req.on('data', (chunk) => {
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('messages.txt', message);
+      fs.writeFile('messages.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
 
   res.setHeader('Content-Type', 'text/html');
