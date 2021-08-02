d5dfd9fe815cdac99b15fcad95c8434ad12a7b67
git-dmitriy <shalberkind@mail.ru>
2021-08-02 16:47:27 +0300

    Redirecting Requests

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
 
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html');
     res.write(
       '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Message</title></head><body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">send</button></form></body></html>'
     );
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('messages.txt', 'something very important');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
 
