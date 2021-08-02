fa46bda7cd1b1cad01df3f1f10137bf41ddc89de
git-dmitriy <shalberkind@mail.ru>
2021-08-02 15:40:18 +0300

    Routing Requests

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers, req.method);
-  // process.exit();
+  const url = req.url;
+
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write(
+      '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Message</title></head><body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">send</button></form></body></html>'
+    );
+    return res.end();
+  }
 
   res.setHeader('Content-Type', 'text/html');
   res.write(
