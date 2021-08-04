624f21168c502980148a2313c577586a3282807b
git-dmitriy <shalberkind@mail.ru>
2021-08-04 23:34:56 +0300

    Adding middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http');
-
+const express = require('express');
 const routes = require('./routes');
+
+const app = express();
 
 const server = http.createServer(routes.handler);
 
