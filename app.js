d469aa70365b6e2528329eb07530e76568ddde7c
git-dmitriy <shalberkind@mail.ru>
2021-08-05 10:31:27 +0300

    Adding middleware

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('In a middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const express = require('express');
-const routes = require('./routes');
-
-const app = express();
-
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
