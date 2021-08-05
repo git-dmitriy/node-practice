a807f079aa83ef82d97f438dbdef39d3d0b13dfa
git-dmitriy <shalberkind@mail.ru>
2021-08-05 16:19:55 +0300

    Adding 404 route

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not foung</h1>');
+});
+
 app.listen(3000);
