e075830fe8b40159c965c233f44cd8581325bfe4
git-dmitriy <shalberkind@mail.ru>
2021-08-05 16:16:15 +0300

    Using express router

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add product</button></form>'
-  );
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('This always runs!');
-  res.send('<h1>This always runs!</h1>');
-  // next();
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
