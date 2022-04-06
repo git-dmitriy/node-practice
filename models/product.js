13d8c85290dca16c8d08ef6d7bdf575d14782596
git-dmitriy <shalberkind@mail.ru>
2022-04-06 23:59:24 +0300

    Storing data in files via model

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const products = [];
 
 module.exports = class Product {
@@ -6,10 +9,27 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+
+    fs.readFile(p, 'utf-8', (err, fileContent) => {
+      let products = [];
+
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+
+      fs.writeFile(p, JSON.stringify(products), (err) =>
+        console.log('err:', err)
+      );
+    });
   }
 
   static fetchAll() {
-    return products;
+    return products ? products : [];
   }
 };
