967f99f12a5940f47371f2d3dd7853aaa8e11d79
git-dmitriy <shalberkind@mail.ru>
2022-04-07 22:54:31 +0300

    Restructure views files

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add product',
     activeAddProduct: true,
     path: '/admin/add-product',
@@ -17,7 +17,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render('shop', {
+    res.render('shop/product-list', {
       prods: products,
       pageTitle: 'Shop',
       hasProducts: products.length > 0,
