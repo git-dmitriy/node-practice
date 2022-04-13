441c021c8ddeee2121adade74e177a890fedb080
git-dmitriy <shalberkind@mail.ru>
2022-04-13 23:44:24 +0300

    Using dynamic path parameters

--- models/product.js
+++ models/product.js
@@ -23,6 +23,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => console.info(err));
@@ -31,5 +32,12 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((product) => product.id === id);
+      cb(product);
+    });
   }
 };
--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 
-router.get('/product-list', shopController.getProducts);
+router.get('/products', shopController.getProducts);
+router.get('/product/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
-// router.get('/product-detail', shopController);
 
 module.exports = router;
